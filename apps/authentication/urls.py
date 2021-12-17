de928c6f00810253f207427f27ed8cd1dd68bfd6
f4rk4sh <farkashyaroslav@gmail.com>
2021-12-17 15:44:38 +0200

    adding registration with comfirmation mail

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import UserView, UserListView, RegisterView
+from .views import UserView, UserListView, signup, activate
 
 app_name = 'apps.authentication'
 
 urlpatterns = [
     path('', UserView.as_view(), name='user'),
     path('users/', UserListView.as_view(), name='users'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', signup, name='register'),
+    path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', activate, name='activate')
 ]
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,8 +1,16 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
-from django.views.generic import TemplateView, FormView
-from .forms import UserCreateForm
+from django.views.generic import TemplateView
+from .forms import SignupForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.core.mail import EmailMessage
 
 User = get_user_model()
 
@@ -25,11 +33,42 @@ class UserListView(LoginRequiredMixin, TemplateView):
         return context
 
 
-class RegisterView(FormView):
-    template_name = 'authentication/register.html'
-    form_class = UserCreateForm
-    success_url = reverse_lazy('authentication:user')
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your blog account.'
+            message = render_to_string('authentication/acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'authentication/register.html', context={'form': form})
 
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
