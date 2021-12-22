22556c0bd1e2dad053e3b803b0e3077310f72458
f4rk4sh <farkashyaroslav@gmail.com>
2021-12-22 22:46:43 +0200

    fixing cbv in authentication/views.py, adding success message in registration form

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, View
+from django.views.generic import TemplateView, View, CreateView
 from .forms import SignupForm
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
@@ -34,29 +34,28 @@ class UserListView(LoginRequiredMixin, TemplateView):
         return context
 
 
-class SignUpView(View):
-    def get(self, request):
-        form = SignupForm()
-        return render(request, 'authentication/register.html', context={'form': form})
+class SignUpView(SuccessMessageMixin, CreateView):
+    template_name = 'authentication/register.html'
+    form_class = SignupForm
+    success_url = reverse_lazy('authentication:register')
+    success_message = 'Please confirm your email address to complete the registration'
 
-    def post(self, request):
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active = False
-            user.save()
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.is_active = False
+        self.object.save()
 
-            current_site = get_current_site(request)
-            body = {'user': user.username,
-                    'domain': current_site,
-                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                    'token': account_activation_token.make_token(user),
-                    }
-            subject = 'Activate your account'
-            message = render_to_string('authentication/acc_active_email.html', body)
-            recipient = form.cleaned_data.get('email')
-            send_verification_email.delay(subject, message, recipient)
-            return HttpResponse('Please confirm your email address to complete the registration')
+        current_site = get_current_site(self.request)
+        body = {'user': self.object.username,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(self.object.pk)),
+                'token': account_activation_token.make_token(self.object),
+                }
+        subject = 'Activate your account'
+        message = render_to_string('authentication/acc_active_email.html', body)
+        recipient = self.object.email
+        send_verification_email.delay(subject, message, recipient)
+        return super().form_valid(form)
 
 
 class ActivateView(View):
